6aa4698dceaafb9c1ad81bd1af02cd52c1009c6c
twiemold <twiemold@uvm.edu>
2021-12-12 10:46:14 -0500

    right answer for part 1, manually converted binaries for now

--- main.cpp
+++ main.cpp
@@ -8,6 +8,8 @@ int main() {
     fstream inputFile;
     vector<string> inputs;
     string input;
+    string gammaRate;
+    string epsilonRate;
     inputFile.open("../input.txt");
     if (inputFile.is_open()) {
         while (inputFile >> input) {
@@ -15,9 +17,28 @@ int main() {
         }
     }
 
-    for (string &i : inputs) {
-        cout << i << endl;
+    for (int i = 0; i < input.size(); ++i) {
+        int zeroBits = 0;
+        int oneBits = 0;
+        for (int j = 0; j < inputs.size(); ++j) {
+            int val = stoi(inputs[j].substr(i,1));
+            if (val == 1) {
+                ++oneBits;
+            } else {
+                ++zeroBits;
+            }
+        }
+        if (oneBits > zeroBits) {
+            gammaRate.append("1");
+            epsilonRate.append("0");
+        } else {
+            epsilonRate.append("1");
+            gammaRate.append("0");
+        }
     }
+
+    cout << "The gamma rate is " + gammaRate << endl;
+    cout << "The epsilon rate is " + epsilonRate << endl;
 
     return 0;
 }
