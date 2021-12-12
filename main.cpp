3d3f5c98a3f4001c46f35ef9e86cd5d4f37364ba
twiemold <twiemold@uvm.edu>
2021-12-12 14:32:13 -0500

    read in draw numbers

--- main.cpp
+++ main.cpp
@@ -1,6 +1,33 @@
 #include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <sstream>
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    fstream inputFile;
+    vector<string> inputs;
+    vector<int> drawNumbers;
+    string input;
+    inputFile.open("../input.txt");
+    if (inputFile.is_open()) {
+        while (inputFile >> input) {
+            inputs.push_back(input);
+        }
+    }
+    inputFile.close();
+
+    stringstream myStream(inputs[0].substr());
+    while (myStream.good()) {
+        string substr;
+        getline(myStream, substr, ',');
+        int val = stoi(substr);
+        drawNumbers.push_back(val);
+    }
+
+    for (int& i : drawNumbers) {
+        cout << i << endl;
+    }
     return 0;
 }
