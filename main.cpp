e08e1f30e7c0e317b2a84b72f930182ed68ab581
twiemold <twiemold@uvm.edu>
2021-12-12 18:17:07 -0500

    right answer for part 1

--- main.cpp
+++ main.cpp
@@ -10,6 +10,9 @@ int main() {
     vector<string> inputs;
     vector<int> drawNumbers;
     string input;
+    int finalScore = 0;
+    int finalDrawNumber = -1;
+    int winningBoard = -1;
     inputFile.open("../input.txt");
     if (inputFile.is_open()) {
         while (inputFile >> input) {
@@ -27,9 +30,7 @@ int main() {
     }
 
     const int numberOfBoards = (inputs.size() - 1) / 25;
-    // Cheating
-    int boards[100][5][5];
-    int markedBoards[100][5][5];
+    int boards[numberOfBoards][5][5];
 
     int l = 1;
     for (int i = 0; i < numberOfBoards; ++i) {
@@ -42,12 +43,56 @@ int main() {
         }
     }
 
+    for (int i = 0; i < drawNumbers.size(); ++i) {
+        int drawNumber = drawNumbers[i];
+        // Mark boards
+        for (int q = 0; q < numberOfBoards; ++q) {
+            for (int j = 0; j < 5; ++j) {
+                for (int k = 0; k < 5; ++k) {
+                    if (boards[q][j][k] == drawNumber) {
+                        boards[q][j][k] = -1;
+                    }
+                }
+            }
+        }
 
 
-    for (int& i : drawNumbers) {
-        cout << i << endl;
+        // Check boards
+        for (int q = 0; q < numberOfBoards; ++q) {
+            for (int j = 0; j < 5; ++j) {
+                int rowSum = 0;
+                int columnSum = 0;
+                for (int k = 0; k < 5; ++k) {
+                    rowSum += boards[q][j][k];
+                    columnSum += boards[q][k][j];
+                }
+                if (rowSum == -5 || columnSum == -5) {
+                    // Bingo !
+                    winningBoard = q;
+                    finalDrawNumber = drawNumber;
+                    break;
+                }
+            }
+            if (winningBoard != -1) {
+                break;
+            }
+        }
+        if (winningBoard != -1) {
+            break;
+        }
     }
 
-    cout << inputs.size();
+    int unmarkedTotal = 0;
+    for (int j = 0; j < 5; ++j) {
+        for (int k = 0; k < 5; ++k) {
+            if (boards[winningBoard][k][j] != -1) {
+                unmarkedTotal += boards[winningBoard][k][j];
+            }
+        }
+    }
+    finalScore = unmarkedTotal * finalDrawNumber;
+
+    cout << "Final winning score is " + to_string(finalScore);
+
     return 0;
 }
