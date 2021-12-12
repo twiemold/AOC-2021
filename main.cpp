1eb4eb01111b92e437385b6f125ba76ec3065fa8
twiemold <twiemold@uvm.edu>
2021-12-12 10:14:05 -0500

    read in data

--- main.cpp
+++ main.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    fstream inputFile;
+    vector<string> inputs;
+    string input;
+    inputFile.open("../input.txt");
+    if (inputFile.is_open()) {
+        while (inputFile >> input) {
+            inputs.push_back(input);
+        }
+    }
+
+    for (string &i : inputs) {
+        cout << i << endl;
+    }
+
     return 0;
 }
