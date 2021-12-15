c3fa7c58f3cfa6cb6a898a410de9dc5a67e1cbc4
twiemold <twiemold@uvm.edu>
2021-12-15 12:10:32 -0500

    read in data

--- main.cpp
+++ main.cpp
@@ -1,6 +1,34 @@
 #include <iostream>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <sstream>
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    fstream inputFile;
+    vector<string> inputs;
+    string input;
+    vector<int> lanternfish;
+    inputFile.open("../input.txt");
+    if (inputFile.is_open()) {
+        while (inputFile >> input) {
+            inputs.push_back(input);
+        }
+    }
+    inputFile.close();
+
+    stringstream myStream(inputs[0].substr());
+    while (myStream.good()) {
+        string substr;
+        getline(myStream, substr, ',');
+        int val = stoi(substr);
+        lanternfish.push_back(val);
+    }
+
+    for (int &i : lanternfish) {
+        cout << i << endl;
+    }
     return 0;
 }
