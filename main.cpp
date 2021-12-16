fd440e8bdeb5ceb9ea1f04b458216025b1693164
twiemold <twiemold@uvm.edu>
2021-12-15 19:46:19 -0500

    read in input

--- main.cpp
+++ main.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <sstream>
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    fstream inputFile;
+    vector<string> inputs;
+    string input;
+    vector<int> crabs;
+    inputFile.open("../input.txt");
+    if (inputFile.is_open()) {
+        while (inputFile >> input) {
+            inputs.push_back(input);
+        }
+    }
+    inputFile.close();
+
+    stringstream myStream(inputs[0].substr());
+    while (myStream.good()) {
+        string substr;
+        getline(myStream, substr, ',');
+        int val = stoi(substr);
+        crabs.push_back(val);
+    }
+
     return 0;
 }
