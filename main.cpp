f7d4d5f7fc484cd40918bb7f4b5cd29829f652c3
twiemold <twiemold@uvm.edu>
2021-12-16 10:53:45 -0500

    read in input

--- main.cpp
+++ main.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <sstream>
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    fstream inputFile;
+    string input;
+    vector<string> signalPatterns;
+    vector<string> outputValues;
+    inputFile.open("../input.txt");
+    if (inputFile.is_open()) {
+        while (inputFile >> input) {
+            getline(inputFile, input, '|');
+            signalPatterns.push_back(input);
+            getline(inputFile, input);
+            outputValues.push_back(input);
+        }
+    }
+    inputFile.close();
+
+    for (string &i : signalPatterns) {
+        cout << i << endl;
+    }
     return 0;
 }
