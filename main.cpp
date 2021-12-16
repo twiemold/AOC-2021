c5bb4c1ebeac898635db86700873ee29e4c550cc
twiemold <twiemold@uvm.edu>
2021-12-16 09:18:40 -0500

    right answer for part 1

--- main.cpp
+++ main.cpp
@@ -10,6 +10,7 @@ int main() {
     vector<string> inputs;
     string input;
     vector<int> crabs;
+    vector<vector<int>> fuelCosts;
     inputFile.open("../input.txt");
     if (inputFile.is_open()) {
         while (inputFile >> input) {
@@ -25,6 +26,37 @@ int main() {
         int val = stoi(substr);
         crabs.push_back(val);
     }
+
+    int max = *std::max_element(crabs.begin(), crabs.end());
+
+    for (int j = 0; j < max; ++j) {
+        vector<int> tempFuelCosts;
+        vector<int> finalValues;
+        int locationVal = j;
+        int fuelSum = 0;
+        for (int crab : crabs) {
+            int fuelCost = abs(crab - j);
+            tempFuelCosts.push_back(fuelCost);
+        }
+        for (int tempFuelCost : tempFuelCosts) {
+            fuelSum += tempFuelCost;
+        }
+        finalValues.push_back(fuelSum);
+        finalValues.push_back(locationVal);
+        fuelCosts.push_back(finalValues);
+    }
+
+    int minFuel = 1000000;
+    int minLocation = -1;
+    for (auto & fuelCost : fuelCosts) {
+        if (fuelCost[0] < minFuel) {
+            minFuel = fuelCost[0];
+            minLocation = fuelCost[1];
+        }
+    }
+
+    cout << "The minimum amount of fuel is " + to_string(minFuel)
+         + " at location " + to_string(minLocation);
 
     return 0;
 }
