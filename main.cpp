c63b9920e3be95a25ec8172f5fad711ad7183acc
twiemold <twiemold@uvm.edu>
2021-12-21 09:55:45 -0500

    right answer for part 2

--- main.cpp
+++ main.cpp
@@ -5,11 +5,13 @@
 #include <sstream>
 using namespace std;
 
+const int numberOfDays = 256;
+
 int main() {
     fstream inputFile;
     vector<string> inputs;
     string input;
-    vector<int> lanternfish;
+    vector<long int> lanternfish(9, 0);
     inputFile.open("../input.txt");
     if (inputFile.is_open()) {
         while (inputFile >> input) {
@@ -18,30 +20,41 @@ int main() {
     }
     inputFile.close();
 
+
     stringstream myStream(inputs[0].substr());
     while (myStream.good()) {
         string substr;
         getline(myStream, substr, ',');
         int val = stoi(substr);
-        lanternfish.push_back(val);
+        lanternfish[val] += 1;
     }
 
-    for (int i = 0; i < 80; ++i) {
-        int newLanternfish = 0;
-        for (int & fish : lanternfish) {
-            if (fish == 0) {
-                ++newLanternfish;
-                fish = 6;
-            } else {
-                --fish;
+    for (int i = 0; i < numberOfDays; ++i) {
+        vector<long int> oldLanternfish(9, 0);
+        for (int j = 0; j < lanternfish.size(); ++j) {
+            oldLanternfish[j] = lanternfish[j];
+        }
+        for (int j = 0; j < lanternfish.size(); ++j) {
+            if (j == 8) {
+                lanternfish[j] = oldLanternfish[0];
+            }
+            else if (j == 6) {
+                lanternfish[j] = oldLanternfish[0];
+                lanternfish[j] += oldLanternfish[j+1];
+            }
+            else {
+                lanternfish[j] = oldLanternfish[j+1];
             }
         }
-        for (int j = 0; j < newLanternfish; ++j) {
-            lanternfish.push_back(8);
-        }
     }
 
-    cout << "There are " + to_string(lanternfish.size()) + " lanternfish after 80 days";
+    unsigned long int sum = 0;
+    for (auto & fish : lanternfish) {
+        sum += fish;
+    }
+
+
+    cout << "There are " + to_string(sum) + " lanternfish after 80 days";
 
     return 0;
 }
